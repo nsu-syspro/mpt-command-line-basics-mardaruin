b4deca2d3622949a7f8c82e8fc55a8eefa58d76d
Marina Rudometova <m.rudometova@g.nsu.ru>
2024-09-13 19:51:41 +0700

    day 2 of me trying to do this task

--- solution/task2.sh
+++ solution/task2.sh
@@ -1,7 +1,6 @@
 cd data
 unzip archive-part2.zip
 tar xf archive-part1.tar
-tar cf archive-combined.tar some
 cd ..
 find ./data -name "foo.txt" -exec cat {} \;
 find ./data -name "bar.txt" -exec cat {} \;
