b3d4102ec8f3ff9da27a1cf0dcd43cfca592ade0
Marina Rudometova <m.rudometova@g.nsu.ru>
2024-09-13 19:49:02 +0700

    day 1 of me trying to do this task

--- solution/task1.sh
+++ solution/task1.sh
@@ -3,5 +3,5 @@ unzip archive-part2.zip
 tar xf archive-part1.tar
 tar cf archive-combined.tar some
 gzip archive-combined.tar
-cd -
+cd ..
 
--- solution/task2.sh
+++ solution/task2.sh
@@ -1,4 +1,8 @@
-bash -xe ./solution/task1.sh
+cd data
+unzip archive-part2.zip
+tar xf archive-part1.tar
+tar cf archive-combined.tar some
+cd ..
 find ./data -name "foo.txt" -exec cat {} \;
 find ./data -name "bar.txt" -exec cat {} \;
 find ./data -name "baz.txt" -exec cat {} \;
