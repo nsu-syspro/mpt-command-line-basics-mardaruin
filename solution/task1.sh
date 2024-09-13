24fa13b80c8092269d9f665318e31d737cef46b5
Marina Rudometova <m.rudometova@g.nsu.ru>
2024-09-13 18:42:35 +0700

    removed from main

--- solution/task1.sh
+++
@@ -1,4 +0,0 @@
-unzip archive-part2.zip
-tar --list -f archive-part1.tar
-tar cf archive-combined.tar some
-gzip archive-combined.tar
--- solution/task2.sh
+++
@@ -1,3 +0,0 @@
-find ./ -name "foo.txt" -exec cat {} \;
-find ./ -name "bar.txt" -exec cat {} \;
-find ./ -name "baz.txt" -exec cat {} \;
